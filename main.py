d90bf0821c162f6fe8cc621d4d540f3001bbbd91
Ayush singh <ayushsingh02082004@gmail.com>
2026-03-26 12:14:31 +0530

    feature uc 5 added

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def main():
     last_name = get_input("Last Name" , Validator.validate_name , "Invalid Last name")
     email = get_input("Email" , Validator.validate_email , "INvalid Email")
     phone = get_input("Phone" , Validator.validate_phone , "Invalid Phone")
+    password = get_input("Password", Validator.validate_password,"Password must have atleast 8 characters")
 
     user = User(first_name , last_name  , email , phone)
 
