8b680241694fc7fc08fe760d3890a194e437ceaf
Ayush singh <ayushsingh02082004@gmail.com>
2026-03-26 10:36:58 +0530

    added feature uc2

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import re
 
 
 class User:
-    def __init__(self, firstname):
+    def __init__(self, firstname , lastname):
         self.firstname = firstname
+        self.lastname = lastname
 
 
     def __str__(self):
-        return f"First Name: {self.firstname}"
+        return f"First Name: {self.firstname} LastName: {self.lastname}"
+    
     
 
 class Validator:
@@ -15,6 +17,11 @@ class Validator:
     def validate_first_name(name):
         pattern = r'^[A-Z][a-z]{2,}$'
         return re.match(pattern, name)
+    
+    @staticmethod
+    def validate_last_name(name):
+        pattern = r"^[A-Z][a-z]{3,}$"
+        return re.match(pattern ,name)
     
 
 
@@ -28,13 +35,19 @@ def get_input(field, validation_func, error_message):
 
 
 def main():
-    first_name = get_input(
+    firstname = get_input(
         "First Name",
         Validator.validate_first_name,
         "First Name should start with a capital letter and have at least 3 characters"
     )
 
-    user = User(first_name)
+    lastname = get_input(
+        "Last Name",
+        Validator.validate_last_name,
+        "Last name should start with a capital letter and should have at least 3 characters"
+    )
+
+    user = User(firstname , lastname)
     print("\nUser Registered Successfully!")
     print(user)
 
